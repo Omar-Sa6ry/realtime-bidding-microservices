910e0725e99f92d69b5a69765ccf5f9836821817
Omar <omarahmedsabry05@gmail.com>
2026-03-30 13:17:41 +0200

    Create Basics of Ai Service and its kub files

---
+++ services/ai-service/src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+        path: join(process.cwd(), 'src/schema.gql'),
+      },
+      playground: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri:
+          configService.get<string>('MONGO_URI') ||
+          'mongodb://ai-db-srv:27017/ai_db',
+      }),
+    }),
+  ],
+})
+export class AppModule {}
---
+++ services/ai-service/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const logger = new Logger('AIService');
+  const app = await NestFactory.create(AppModule);
+  
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  
+  const port = process.env.PORT || 3005;
+  await app.listen(port);
+  logger.log(`AI Service is running on port ${port}`);
+}
+bootstrap();
