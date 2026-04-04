325de2ccb84ed448cb7955c4c5ad2ba45de625d9
Omar <omarahmedsabry05@gmail.com>
2026-04-04 06:30:11 +0200

    ِAdd unit tests for UserService methods ( findByIds, findByEmail, findUsers & getUsersCount )

--- services/user-service/src/modules/users/users.service.spec.ts
+++ services/user-service/src/modules/users/users.service.spec.ts
@@ -6,6 +6,7 @@ import { Transaction } from './entity/transaction.entity';
 import { RedisService } from '@bts-soft/core';
 import { I18nService } from 'nestjs-i18n';
 import { NotFoundException } from '@nestjs/common';
+import { In } from 'typeorm';
 
 jest.mock('typeorm-transactional', () => ({
   Transactional: () => () => ({}),
@@ -106,6 +107,140 @@ describe('UserService', () => {
       await expect(service.findById('invalid-id')).rejects.toThrow(
         'translated: user.NOT_FOUND',
       );
+    });
+  });
+
+  describe('findByIds', () => {
+    it('should return empty array if no ids are provided', async () => {
+      const result = await service.findByIds([]);
+      expect(result).toEqual([]);
+      expect(mockUserRepository.find).not.toHaveBeenCalled();
+    });
+
+    it('should return users if ids are provided', async () => {
+      const users = [{ id: 'user-1', email: 'omar@test.com' }];
+      mockUserRepository.find.mockResolvedValueOnce(users);
+
+      const result = await service.findByIds(['user-1']);
+      expect(result).toEqual(users);
+      expect(mockUserRepository.find).toHaveBeenCalledWith({
+        where: { id: In(['user-1']) },
+      });
+    });
+  });
+
+  describe('findByEmail', () => {
+    it('should return user from redis cache if available', async () => {
+      const cachedUser = { id: 'user-1', email: 'omar@test.com' };
+      mockRedisService.get.mockResolvedValueOnce({ data: cachedUser });
+
+      const result = await service.findByEmail('omar@test.com');
+      expect(result.data).toEqual(cachedUser);
+      expect(mockRedisService.get).toHaveBeenCalledWith(
+        'user:email:omar@test.com',
+      );
+      expect(mockUserRepository.findOne).not.toHaveBeenCalled();
+    });
+
+    it('should fetch from DB if not in cache, then update cache', async () => {
+      const dbUser = { id: 'user-2', email: 'db@db.com' };
+      mockRedisService.get.mockResolvedValueOnce(null);
+      mockUserRepository.findOne.mockResolvedValueOnce(dbUser);
+
+      const result = await service.findByEmail('db@db.com');
+      expect(result.data).toEqual(dbUser);
+      expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+        where: { email: 'db@db.com' },
+      });
+      expect(mockRedisService.set).toHaveBeenCalled();
+    });
+
+    it('should throw NotFoundException if user not found in Cache or DB', async () => {
+      mockRedisService.get.mockResolvedValueOnce(null);
+      mockUserRepository.findOne.mockResolvedValueOnce(null);
+
+      await expect(service.findByEmail('invalid@email.com')).rejects.toThrow(
+        NotFoundException,
+      );
+      await expect(service.findByEmail('invalid@email.com')).rejects.toThrow(
+        'translated: user.NOT_FOUND',
+      );
+    });
+  });
+
+  describe('findUsers', () => {
+    it('should return users with pagination', async () => {
+      const users = [{ id: 'user-1', email: 'omar@test.com' }];
+      mockUserRepository.findAndCount.mockResolvedValueOnce([users, 1]);
+
+      const result = await service.findUsers();
+      expect(result.items).toEqual(users);
+      expect(result.pagination).toEqual({
+        totalItems: 1,
+        currentPage: 1,
+        totalPages: 1,
+      });
+    });
+
+    it('should return users with pagination and filters', async () => {
+      const users = [{ id: 'user-1', email: 'omar@test.com' }];
+      mockUserRepository.findAndCount.mockResolvedValueOnce([users, 1]);
+
+      const result = await service.findUsers(1, 10);
+      expect(result.items).toEqual(users);
+      expect(result.pagination).toEqual({
+        totalItems: 1,
+        currentPage: 1,
+        totalPages: 1,
+      });
+    });
+  });
+
+  describe('getUsersCount', () => {
+    it('should return users count from redis cache if available', async () => {
+      const cachedCount = {
+        totalUsers: 10,
+        usersThisMonth: 5,
+        percentageIncrease: 10,
+      };
+      mockRedisService.get.mockResolvedValueOnce(cachedCount);
+
+      const result = await service.getUsersCount();
+      expect(result.data).toEqual(cachedCount);
+      expect(mockRedisService.get).toHaveBeenCalledWith('user-count');
+      expect(mockUserRepository.count).not.toHaveBeenCalled();
+    });
+
+    it('should fetch from DB if not in cache, then update cache', async () => {
+      mockRedisService.get.mockResolvedValueOnce(null);
+
+      mockUserRepository.count.mockResolvedValueOnce(10);
+      mockUserRepository.count.mockResolvedValueOnce(5);
+      mockUserRepository.count.mockResolvedValueOnce(0);
+
+      const result = await service.getUsersCount();
+
+      expect(result.data).toEqual({
+        totalUsers: 10,
+        usersThisMonth: 5,
+        percentageIncrease: 100,
+      });
+      expect(mockRedisService.set).toHaveBeenCalled();
+    });
+
+    it('should calculate percentage increase correctly when last month has users', async () => {
+      mockRedisService.get.mockResolvedValueOnce(null);
+
+      mockUserRepository.count.mockResolvedValueOnce(10);
+      mockUserRepository.count.mockResolvedValueOnce(2);
+      mockUserRepository.count.mockResolvedValueOnce(1);
+      const result = await service.getUsersCount();
+
+      expect(result.data).toEqual({
+        totalUsers: 10,
+        usersThisMonth: 2,
+        percentageIncrease: 100,
+      });
     });
   });
 });
